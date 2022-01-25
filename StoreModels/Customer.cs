c50ff145979e98c66ccc0f9cf9f45cce50d4110e
LornaGrajek <lorna.grajek@gmail.com>
2022-01-24 16:28:00 -0800

    added some tests, exception handling, and validation

--- StoreModels/Customer.cs
+++ StoreModels/Customer.cs
@@ -1,5 +1,8 @@
 global using System.Data;
 namespace Models;
+using System.Text.RegularExpressions;
+using CustomExceptions;
+
 
 public class Customer
 {
@@ -14,7 +17,28 @@ public class Customer
         this.Password = row["PassWord"].ToString() ?? "";
     }
     public int CID { get; set; }
-    public string UserName { get; set; }
+    // public string UserName { get; set; }
+    private string _UserName;
+    public string UserName 
+    {
+        get => _UserName;
+        set
+        {
+            Regex pattern = new Regex("^[a-zA-Z0-9 !?']+$");
+            if(string.IsNullOrWhiteSpace(value))
+            {
+                throw new InputInvalidException("Name can't be empty");
+            }
+            else if(!pattern.IsMatch(value))
+            {
+                throw new InputInvalidException("Restaurant name can only have alphanumeric characters, white space, !, ?, and '.");
+            }
+            else
+            {
+                this._UserName = value;
+            }
+        }
+    }
     public string Password { get; set; }
     public List<Order> Orders { get; set; }
 }
